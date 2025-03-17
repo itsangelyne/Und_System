b26baff15b316bf88e86b301f1167786065a7f9c
ange <itsangelyne14@gmail.com>
2025-03-17 15:01:45 +0800

    hello ud

---
+++ Und_System/Data/CGL_WithNotice.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Und_System.Data
+{
+    public class CGL_WithNotice
+    {
+        [Key]
+        public int NoticesID { get; set; }
+        public int ID_Insured { get; set; }
+        public DateTime Letter_Date { get; set; }
+        public int Ref_No { get; set; }
+        public string? Type_Of_Notice { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
