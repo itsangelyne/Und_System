cc074e55e3493ded1c571cbb59f8492e304ea12b
ange <itsangelyne14@gmail.com>
2025-05-19 15:01:49 +0800

    To Do: 1. LOGIN    1.1 If mismatch, mamumula rin yung boarder if possible.    1.2 If wala sa database then tried sa paglogin, mageerror (Display: No Usernames matched or something)    1.3 Reset Password through email (try na gawin)
    2. DASHBOARD
       2.1 yung aplication dropdown bhie ayusin mo nga

--- Und_System/Services/CGLUnderwritingUserService.cs
+++ Und_System/Services/CGLUnderwritingUserService.cs
@@ -14,11 +14,13 @@ namespace Und_System.Services
             _context = context;
         }
 
+        //To List Users
         public async Task<List<CGL_UnderwritingUser>> GetUserAsync()
         {
             return await _context.CGL_UnderwritingUser.ToListAsync();
         }
 
+        // To Update User Status (Active or Inactive)
         public async Task UpdateUserStatusAsync(CGL_UnderwritingUser users)
         {
             var existingUser = await _context.CGL_UnderwritingUser.FindAsync(users.UserID);
@@ -27,6 +29,11 @@ namespace Und_System.Services
                 existingUser.Status = users.Status;
                 await _context.SaveChangesAsync();
             }
+        }
+        // To Validate User Login
+        public async Task<CGL_UnderwritingUser> ValidateUserAsync(string Username, string Password)
+        {
+            return await _context.CGL_UnderwritingUser.FirstOrDefaultAsync(u => u.Username == Username && u.Password == Password);
         }
     }
 }
